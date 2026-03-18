2f5b55adcbf45802142b7543f9c2ac8b6fa84362
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-03-18 17:27:48 +0545

    AGGREGATE FUNCTION

--- mediumhospitaldatabase.sql
+++ mediumhospitaldatabase.sql
@@ -27,3 +27,22 @@ where a.diagnosis='Dementia';
 SELECT first_name
 FROM patients
 ORDER BY LENGTH(first_name),first_name ASC;
+
+--6.Show the total amount of male patients and total amount of female patients in patients table.
+--Display the two results in same row.
+SELECT COUNT(*) FILTER (WHERE gender='M') AS male_count,
+COUNT(*) FILTER (WHERE gender='F') AS female_count
+FROM patients;
+
+--7.Show first and last name,allergies from patients which have allergies to either 'Penicillin' or 'Morphine'.Show results ordered ascending by allergies then by first_name then by last_name.
+SELECT first_name,last_name,allergies
+FROM patients
+WHERE allergies IN ('Penicillin','Morphine')
+ORDER BY allergies,first_name,last_name ASC;
+
+--8.Show patient_id,diagnosis from admissions.Find patients admitted multiple times for the same diagnosis.
+SELECT patient_id,diagnosis
+FROM admissions
+GROUP BY patient_id,diagnosis
+HAVING COUNT(*)>1;
+
