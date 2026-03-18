5a2dfd2ad581d303f19ec2cb2f5ec7ab3b417a0b
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-03-18 14:28:04 +0545

    Distinct, Count, Range

--- easyhospitaldatabase.sql
+++ easyhospitaldatabase.sql
@@ -40,3 +40,32 @@ from patients;
 --10.Show all columns for patients who have one of the following patient_ids:1,45,534,879,1000
 SELECT *from patients 
 where patient_id IN (1,45,534,879,1000);
+
+--11.Show the total number of admissions
+SELECT COUNT(patient_id) as total_admissions
+from admissions;
+
+--12.Show all the columns from admissions where the patient was admitted and discharged on the same day.
+SELECT *FROM admissions
+where admission_date=discharge_date;
+
+--13.Show the patient id and the total number of admissions for patient_id 579.
+SELECT patient_id,COUNT(patient_id) as total_admissions
+from admissions
+where patient_id=579;
+
+--14.Based on the cities that our patients live in, show unique cities that are in province_id 'NS'.
+Select DISTINCT city as unique_cities
+from patients
+where province_id='NS';
+
+--15.Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70
+Select first_name,last_name,birth_date
+FROM patients
+WHERE height>160 and weight>70;
+
+--16.Write a query to find list of patients first_name, last_name, and allergies where allergies are not null and are from the city of 'Hamilton'
+SELECT first_name,last_name,allergies
+FROM patients
+where allergies IS NOT NULL
+AND CITY='Hamilton';
