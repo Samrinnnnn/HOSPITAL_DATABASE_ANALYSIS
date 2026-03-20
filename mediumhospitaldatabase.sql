eb341276102fd7bbd82416475ddc93ff0c724c7f
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-03-20 15:10:11 +0545

    GROUP BY, HAVING

--- mediumhospitaldatabase.sql
+++ mediumhospitaldatabase.sql
@@ -95,3 +95,9 @@ HAVING sum_height>=7000;
   FROM admissions
   GROUP BY DAY(admission_date)
   ORDER BY number_of_admissions DESC;
+
+--17.Show all columns for patient_id 542's most recent admission_date.
+SELECT *FROM admissions
+WHERE patient_id=542
+GROUP  BY patient_id
+HAVING admission_date=MAX(admission_date);
