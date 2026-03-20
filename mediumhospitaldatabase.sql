fa4fd1e2bd0f7283efbeab0b0ddeea73af83ecaf
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-03-20 15:30:54 +0545

    CONCAT

--- mediumhospitaldatabase.sql
+++ mediumhospitaldatabase.sql
@@ -113,5 +113,12 @@ FROM admissions
 WHERE (patient_id %2 <> 0 AND attending_doctor_id IN (1,5,19)) OR
 (attending_doctor_id LIKE '%2%' AND LENGTH(patient_id)=3);
 
+/*We want to display each patient's full name in a single column.
+Their last_name in all upper letters must appear first,then first_name
+in all lower case letters.Separate the last_name and first_name with a 
+comma.Order the list by the first_name in descending order. */
+SELECT UPPER(last_name)||','|| LOWER(first_name) AS new_name_format
+FROM patients
+ORDER BY first_name DESC;
 
 
